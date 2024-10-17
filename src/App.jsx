234595f8b6be60965d3b1cc3353bd63538193359
Dhineshkumar1202 <167614531+Dhineshkumar1202@users.noreply.github.com>
2024-10-17 11:27:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import StudentDashboard from './pages/StudentDashboard';
+import StudentDashboard from './pages/studentDashboard';
 import AdminDashboard from './pages/AdminDashboard';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
