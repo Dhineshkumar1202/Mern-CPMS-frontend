436d7511070f67f861f89739e13daa613f16bd5c
Dhineshkumar1202 <167614531+Dhineshkumar1202@users.noreply.github.com>
2024-10-28 12:13:50 +0530

    Update applicationService.js

--- src/services/applicationService.js
+++ src/services/applicationService.js
@@ -4,7 +4,7 @@ const API_URL = 'https://my-project-backend-ssaj.vercel.app/api/applications/';
 
 export const submitApplication = async (applicationData) => {
     try {
-        const response = await axios.post(`${API_URL}/applications`, applicationData);
+        const response = await axios.post(`https://my-project-backend-ssaj.vercel.app/api/applications`, applicationData);
         return response.data;
     } catch (error) {
         throw new Error(error.response.data.message || 'Something went wrong');
