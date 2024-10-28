d006e7ce3bb35c7aa1fd9f0e8f7cb9a95a772596
Dhineshkumar1202 <167614531+Dhineshkumar1202@users.noreply.github.com>
2024-10-28 11:30:02 +0530

    Update applicationService.js

--- src/services/applicationService.js
+++ src/services/applicationService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/applications/'; 
+const API_URL = 'https://my-project-backend-ssaj.vercel.app/api/applications/'; 
 
 export const submitApplication = async (applicationData) => {
     try {
